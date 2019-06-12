d792c543bea0fb0870170b504dcf55c5dba36f43
Mateo <mateopuna98@gmail.com>
2019-06-12 17:16:11 -0400

    Anadidos features para editar y eliminar cuenta, clases AccountCreator, NavBar y AccountSettings, y los stepDefs correspondientes

--- src/test/java/uitesting/upb/org/handlewebsite/LoadPage.java
+++ src/test/java/uitesting/upb/org/handlewebsite/LoadPage.java
@@ -1,14 +1,19 @@
 package uitesting.upb.org.handlewebsite;
 
 import uitesting.upb.org.managefile.PropertyAccessor;
+import uitesting.upb.org.managepage.wallet.AccountCreator;
 import uitesting.upb.org.managepage.wallet.AccountManager;
 import uitesting.upb.org.webdrivermanager.DriverManager;
 
 public class LoadPage {
 
-    public static AccountManager loadAccountManager(){
+    public static AccountManager loadAccountManager() {
         DriverManager.getInstance().getWebDriver().navigate().to(PropertyAccessor.getInstance().getBaseURL());
         return new AccountManager();
+    }
+
+    public static AccountCreator loadAccountCreator() {
+        return new AccountCreator();
     }
 
 }
---
+++ src/test/java/uitesting/upb/org/managepage/wallet/AccountSettings.java
@@ -0,0 +1,34 @@
+package uitesting.upb.org.managepage.wallet;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import uitesting.upb.org.manageevents.Events;
+import uitesting.upb.org.managepage.BasePage;
+
+public class AccountSettings extends BasePage {
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div/div/form/div[1]/input")
+    private WebElement changeAccountNameTextField;
+
+    @FindBy(id = "changeNameButton")
+    private WebElement changeNameButton;
+
+    @FindBy(id = "deleteAccountButton")
+    private WebElement deleteAccountButton;
+
+    public void clickChangeNameButton() {
+        Events.click(changeNameButton);
+    }
+
+    public void clickDeleteAccountButton() {
+        Events.click(deleteAccountButton);
+    }
+
+    public void writeInChangeAccountNameTextField(String nombreCuenta) {
+        Events.fillField(changeAccountNameTextField, nombreCuenta);
+    }
+
+    public boolean windowIsVisible() {
+        return Events.isVisibleWebElement(changeAccountNameTextField);
+    }
+}
