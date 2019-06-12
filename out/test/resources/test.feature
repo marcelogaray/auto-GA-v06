8326b9bb530adcb6605a3a1dc621dc63ba7852b5
Adriana <adrianaorellana34@gmail.com>
2019-06-12 11:48:17 -0400

    account creator features added

--- out/test/resources/test.feature
+++ out/test/resources/test.feature
@@ -1,7 +1,11 @@
 Feature: Check account creator
-  #Background: Preconditions
-
+  Background: Preconditions
+    Given 'Account creator' is loaded
 
   Scenario: Check if text input field and add button are visible
-    Given 'Account creator' is loaded
-   # Then the 'Account name' text field and 'add' button is visible in 'Account Creator'
+    Then the 'Account name' text field and 'add' button is visible in 'Account Creator'
+
+  Scenario: Check if new account is added to accounts list
+    And fill "Savings" 'Account name' text field on 'Account Creator'
+    And press 'Add' button on 'Account Creator'
+    Then "Savings" button is visible on 'Account Creator'
--- src/test/java/test_definitions/MyStepdefs.java
+++ src/test/java/test_definitions/MyStepdefs.java
@@ -1,17 +1,48 @@
 package test_definitions;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.testng.Assert;
 import uitesting.upb.org.managepage.wallet.AccountCreator;
+import uitesting.upb.org.managepage.wallet.AccountHome;
 
 import static uitesting.upb.org.handlewebsite.LoadPage.loadAccountCreator;
 
 public class MyStepdefs {
 
+    // No se olviden eliminar espacios innecesarios y subir hasta antes de las 5
+    // En account creator esta un ejemplo de la estructura para cada linea en los features_
+    // accion objeto lugar (en ingles)
+
     private AccountCreator accountCreator;
+    private AccountHome accountHome;
 
     @Given("^'Account creator' is loaded$")
     public void accountCreatorIsLoaded() {
         accountCreator = loadAccountCreator();
+    }
 
+    @Then("^the 'Account name' text field and 'add' button is visible in 'Account Creator'$")
+    public void theAccountNameTextFieldAndAddButtonIsVisibleInAccountCreator() {
+        boolean isAccountNameTextFieldAndAddButtonIsVisible = accountCreator.isAccountNameFieldAndButtonVisible();
+        Assert.assertTrue(isAccountNameTextFieldAndAddButtonIsVisible);
+    }
+
+
+    @And("^fill \"([^\"]*)\" 'Account name' text field on 'Account Creator'$")
+    public void fillAccountNameTextFieldOnAccountCreator(String newAccountName) throws Throwable {
+        accountCreator.fillAccountNameTextField(newAccountName);
+    }
+
+    @And("^press 'Add' button on 'Account Creator'$")
+    public void pressAddButtonOnAccountCreator() {
+        accountCreator.clickAddButton();
+    }
+
+    @Then("^\"([^\"]*)\" button is visible on 'Account Creator'$")
+    public void buttonIsVisibleOnAccountCreator(String accountName) {
+        boolean isVisibleAccountButton = accountCreator.isAccountNameButtonVisible(accountName);
+        Assert.assertTrue(isVisibleAccountButton);
     }
 }
