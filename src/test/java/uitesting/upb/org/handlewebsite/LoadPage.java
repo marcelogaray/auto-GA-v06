ce24b444ef8047269ce98fa54709c3740ee0f3a3
Mateo <mateopuna98@gmail.com>
2019-06-24 17:33:26 -0400

    Anadidos 7 test cases

--- src/test/java/uitesting/upb/org/handlewebsite/LoadPage.java
+++ src/test/java/uitesting/upb/org/handlewebsite/LoadPage.java
@@ -1,9 +1,7 @@
 package uitesting.upb.org.handlewebsite;
 
 import uitesting.upb.org.managefile.PropertyAccessor;
-import uitesting.upb.org.managepage.pagesexamen2.AdidasNavBar;
-import uitesting.upb.org.managepage.pagesexamen2.ShoeButton;
-import uitesting.upb.org.managepage.pagesexamen2.ShoeDescription;
+
 import uitesting.upb.org.managepage.wallet.AccountCreator;
 import uitesting.upb.org.managepage.wallet.AccountManager;
 import uitesting.upb.org.webdrivermanager.DriverManager;
@@ -16,20 +14,10 @@ public class LoadPage {
         return new AccountManager();
     }
 
-    public static AdidasNavBar loadAdidasNavBar(){
-        DriverManager.getInstance().getWebDriver().manage().window().maximize();
-        DriverManager.getInstance().getWebDriver().navigate().to(PropertyAccessor.getInstance().getBaseURL());
-            return new AdidasNavBar();
-    }
-
     public static AccountCreator loadAccountCreator() {
         return new AccountCreator();
     }
 
-    public static ShoeButton loadShoeButton(){
-        DriverManager.getInstance().getWebDriver().navigate().to(PropertyAccessor.getInstance().getBaseURL());
 
-        return new ShoeButton();
-    }
 
 }
